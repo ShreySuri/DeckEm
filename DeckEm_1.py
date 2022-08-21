2db9b2218c6b398e0b1d13cf88ad258b2cdcfc99
ShreySuri <shreysuri5@gmail.com>
2022-08-20 22:19:43 -0500

    Update DeckEm_1.py

--- DeckEm_1.py
+++ DeckEm_1.py
@@ -12,10 +12,8 @@ print("Welcome to Deck 'Em, a solitaire-style boxing game. Check it out on the A
 
 avatar = 21
 
-
-
-blocks = [2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 9, 10] # 12 total
-champs = [2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 12, 13, 14, 15] # 18 total
+blocks = ["2-b", "3-b", "4-b", "5-b", "5-b", "6-b", "6-b", "7-b", "7-b", "8-b", "9-b", "10-b"] # 12 total
+champs = ["2-c", "3-c", "4-c", "5-c", "6-c", "7-c", "7-c", "8-c", "8-c", "9-c", "9-c", "10-c", "10-c", "11-c", "12-c", "13-c", "14-c", "15-c"] # 18 total
 kits = [2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 9, 10] # 12 total
 punches = [3, 3, 3, 3, 3, 5, 5, 5, 15, 15] # 10 total
 
