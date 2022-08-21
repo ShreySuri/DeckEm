22d68c6bbfbb289cfb0d6313310c666e8bd0391e
ShreySuri <shreysuri5@gmail.com>
2022-08-20 22:08:09 -0500

    Update DeckEm_1.py

---
+++ DeckEm_1.py
@@ -0,0 +1,18 @@
+def format_8(list_8):
+    for i in range (0, 2):
+        message = ""
+        for j in range (0, 4):
+            num = 2 * i + j
+            val = list_8[num]
+            message = "%s%s   " % (message, val)
+        print(message)
+    return(None)
+
+print("Welcome to Deck 'Em, a solitaire-style boxing game. Check it out on the App Store! ")
+
+avatar = 21
+blocks = [2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 9, 10] # 12 total
+champs = [2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 12, 13, 14, 15] # 18 total
+kits = [2, 3, 4, 5, 5, 6, 6, 7, 7, 8, 9, 10] # 12 total
+punches = [3, 3, 3, 3, 3, 5, 5, 5, 15, 15] # 10 total 
+            
